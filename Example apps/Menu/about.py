f89a55b8bd7db85fc344af4f64849be9a616c6ce
1nf4mous <james.gratchoff@gmail.com>
2021-10-27 13:35:35 -0700

    About modified

--- Example apps/Menu/about.py
+++ Example apps/Menu/about.py
@@ -11,7 +11,27 @@ WRAP_WIDTH = 320
 io_expander = IOExpander(board.I2C())
 
 about_text = (
-    "OOO 2021 Space Cows has been made possible by:\n\n"
+    "The green forest of belgium has been the place \n"
+    "where the secure team reunites. This is where \n"
+    "COWorkers come together, hack around, play games, \n"
+    "host workshops in a place were they can be their \n"
+    "true self. In other words this is where the\n"
+    "magic happens.\n\n"
+
+    "A year ago, in october 2020 we attempted to gather\n"
+    "the herd but where cut short of an opportunity due\n"
+    "to a threat which prevented us to travel through\n"
+    "space.\n"
+    "This year we reinvented ourselves as SPACE COWS, a\n"
+    "group of geeky cows hacking their way to the MOOOn.\n\n"
+
+    "We are really proud to have spend many hours\n"
+    "ruminating about creating a cooler badge than last\n"
+    "year and we are exctactic about the result.\n"
+    "This year we give you a personal rocket to bring\n"
+    "you to the MOOOn!\n\n"
+
+    "OOO 2021 Space Cows has been made possible by:\n"
     "- Cedric van Bockhaven\n"
     "- Christiaan van den Boogaard\n"
     "- Anneloes Geerts\n"
@@ -21,7 +41,7 @@ about_text = (
     "- David Haines\n"
     "- Rikkert ten Klooster\n"
     "- Pavlos Lontorfos\n"
-    "\nWith special thanks to:\n"
+    "With special thanks to:\n"
     "- Niels van der Vorle\n"
     "- Burc Yildirim\n"
 )
@@ -30,14 +50,32 @@ about_label = None
 
 display = board.DISPLAY
 group = displayio.Group()
+current_y = 10
+move_speed = 340
 
 def build_label():
     about_label = label.Label(terminalio.FONT, text="\n"\
     .join(wrap_text_to_pixels(about_text, WRAP_WIDTH, terminalio.FONT)), color=0xFFFFFF)
 
     about_label.anchor_point = (0.0, 0.0)
-    about_label.anchored_position = (10, 10)
+    about_label.anchored_position = (10, current_y)
     return about_label
+
+def move_down():
+    global current_y
+    
+    if about_label.anchored_position[1] < -100:
+        return
+
+    current_y = current_y - move_speed
+    about_label.anchored_position = (10, current_y)
+    
+def move_up():
+    global current_y
+    if about_label.anchored_position[1] > 0:
+        return
+    current_y = current_y + move_speed
+    about_label.anchored_position = (10, current_y)
 
 def show_about(_):
     global about_label
@@ -50,6 +88,9 @@ def show_about(_):
 
     while True:
         io_expander.update()
-
+        if io_expander.button_down.fell:
+            move_down()
+        if io_expander.button_up.fell:
+            move_up()        
         if io_expander.button_menu.fell:
             return
